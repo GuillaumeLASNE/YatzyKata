0f23260a948127a6a75e6bda357a7d696254d5f5
Guillaume LASNE <guillaume.lasne@lacombedulionvert.fr>
2019-09-23 11:02:57 +0200

    Revert "Simplified one pair logic"
    This reverts commit 8e74a1a0

--- src/main/java/fr/lacombe/yatzy/Yatzy.java
+++ src/main/java/fr/lacombe/yatzy/Yatzy.java
@@ -1,73 +1,65 @@
 package fr.lacombe.yatzy;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
-public class Roll {
+public class Yatzy {
 
     private static final int DIE_FACES_NUMBER = 6;
-    private int[] roll;
+    private int[] dice;
 
-    public Roll(int d1, int d2, int d3, int d4, int d5) {
-        roll = new int[5];
-        roll[0] = d1;
-        roll[1] = d2;
-        roll[3] = d4;
-        roll[2] = d3;
-        roll[4] = d5;
+    public Yatzy(int d1, int d2, int d3, int d4, int d5) {
+        dice = new int[5];
+        dice[0] = d1;
+        dice[1] = d2;
+        dice[3] = d4;
+        dice[2] = d3;
+        dice[4] = d5;
     }
 
     public int chance() {
-        return Arrays.stream(roll).sum();
+        return Arrays.stream(dice).sum();
     }
 
     public int yatzy() {
-        return this.isYatzy() ? 50 : 0;
+        return isYatzy(this.dice) ? 50 : 0;
     }
 
     public int ones() {
-        return sumDiceWithSameValueAs(1, this.roll);
+        return sumDiceWithSameValueAs(1, this.dice);
     }
 
     public int twos() {
-        return sumDiceWithSameValueAs(2, this.roll);
+        return sumDiceWithSameValueAs(2, this.dice);
     }
 
     public int threes() {
-        return sumDiceWithSameValueAs(3, this.roll);
+        return sumDiceWithSameValueAs(3, this.dice);
     }
 
     public int fours() {
-        return sumDiceWithSameValueAs(4, this.roll);
+        return sumDiceWithSameValueAs(4, this.dice);
     }
 
     public int fives() {
-        return sumDiceWithSameValueAs(5, this.roll);
+        return sumDiceWithSameValueAs(5, this.dice);
     }
 
     public int sixes() {
-        return sumDiceWithSameValueAs(6, this.roll);
+        return sumDiceWithSameValueAs(6, this.dice);
     }
 
     public int onePair() {
-        List<Integer> decreasingOrderRoll = getDiceByDecreasingOrder();
-        int offsetForPair = 1;
-
-        for (int i = 0; i < decreasingOrderRoll.size() - offsetForPair; i++) {
-            int currentDie = decreasingOrderRoll.get(i);
-            int nextDie = decreasingOrderRoll.get(i + offsetForPair);
-
-            if (currentDie == nextDie) {
-                return currentDie + nextDie;
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
+        for (int dieValue = DIE_FACES_NUMBER; dieValue >= 1; dieValue--) {
+            if (dieOccurrences.isPair(dieValue)) {
+                return dieValue * 2;
             }
         }
         return 0;
     }
 
     public int twoPair() {
-        DieOccurrences dieOccurrences = new DieOccurrences(this.roll, DIE_FACES_NUMBER);
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
         int score = 0;
         int numberOfPair = 0;
 
@@ -83,7 +75,7 @@ public class Roll {
     }
 
     public int threeOfAKind() {
-        DieOccurrences dieOccurrences = new DieOccurrences(this.roll, DIE_FACES_NUMBER);
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
         for (int dieValue = 1; dieValue <= DIE_FACES_NUMBER; dieValue++) {
             if (dieOccurrences.isThreeOfAKind(dieValue)) {
                 return dieValue * 3;
@@ -93,7 +85,7 @@ public class Roll {
     }
 
     public int fourOfAKind() {
-        DieOccurrences dieOccurrences = new DieOccurrences(this.roll, DIE_FACES_NUMBER);
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
         for (int die = 1; die <= DIE_FACES_NUMBER; die++) {
             if (dieOccurrences.isFourOfAKind(die)) {
                 return die * 4;
@@ -103,21 +95,21 @@ public class Roll {
     }
 
     public int smallStraight() {
-        DieOccurrences dieOccurrences = new DieOccurrences(this.roll, DIE_FACES_NUMBER);
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
         if (dieOccurrences.isSmallStraight())
             return 15;
         return 0;
     }
 
     public int largeStraight() {
-        DieOccurrences dieOccurrences = new DieOccurrences(this.roll, DIE_FACES_NUMBER);
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
         if (dieOccurrences.isLargeStraight())
             return 20;
         return 0;
     }
 
     public int fullHouse() {
-        DieOccurrences dieOccurrences = new DieOccurrences(this.roll, DIE_FACES_NUMBER);
+        DieOccurrences dieOccurrences = new DieOccurrences(this.dice, DIE_FACES_NUMBER);
         boolean hasThreeOfAKind = false;
         boolean hasPair = false;
         int score = 0;
@@ -140,14 +132,7 @@ public class Roll {
                 .sum();
     }
 
-    private boolean isYatzy() {
-        return Arrays.stream(roll).allMatch(die -> roll[0] == die);
-    }
-
-    private List<Integer> getDiceByDecreasingOrder() {
-        return Arrays.stream(roll)
-                .boxed()
-                .sorted(Collections.reverseOrder())
-                .collect(Collectors.toList());
+    private boolean isYatzy(int[] dice) {
+        return Arrays.stream(dice).allMatch(die -> dice[0] == die);
     }
 }
